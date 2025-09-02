a6860d83ddcf3bb3e9884eeee30a4cfe5418fa1a
mannyman123 <64105022+mannyman123@users.noreply.github.com>
2025-09-02 17:58:04 -0400

    Add files via upload

---
+++ age.cpp
@@ -0,0 +1,12 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int age;
+  cout << "How old are you my man?";
+  cin >> age;
+  cout << "I am " << age  "years old." << endl;
+  return 0;
+}
