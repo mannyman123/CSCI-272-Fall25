22dfe777085311f49fd88919068962076db8d22d
mannyman123 <64105022+mannyman123@users.noreply.github.com>
2025-09-02 18:07:28 -0400

    Add files via upload

---
+++ addition.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int num1, num2, sum;
+  cout << "Write the first number brodie:";
+  cin >> num1; 
+  cout << "Write the second number brodie:";
+  cin >> num2;
+  sum = num1 + num2;
+  cout << "The sum is: " << sum << endl;
+  
+  return 0;
+  
+}
+
